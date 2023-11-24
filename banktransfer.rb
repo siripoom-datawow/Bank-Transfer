d50aea71638d79594e50d8169dd912d58516b34f
Siripoom Luengsrisakul <siripoom.l@datawow.io>
2023-11-24 07:56:29 +0700

    refactor: Rubocop autocorrect and set syntax to conventional form

--- banktransfer.rb
+++ banktransfer.rb
@@ -1,36 +1,38 @@
+# frozen_string_literal: true
+
 class BankTransfer
-  def initialize (transaction,amount)
+  def initialize(transaction, amount)
     @transaction = transaction.upcase
     @amount = amount.dup
   end
+
   def min_initial_amount
-    raise ArgumentError, "No transaction data"  if @transaction.empty?
-      raise ArgumentError, "invalid argument"  if @transaction.length != @amount.length
+    raise ArgumentError, 'No transaction data' if @transaction.empty?
+    raise ArgumentError, 'invalid argument' if @transaction.length != @amount.length
+
     sum_a = 0
     sum_b = 0
     result_a = 0
     result_b = 0
-    @transaction.split('').each_with_index do |t,index|
+    @transaction.split('').each_with_index do |t, index|
       case t
-      when "B"
+      when 'B'
         sum_a -= @amount[index]
         sum_b += @amount[index]
-        unless sum_a > 0
+        unless sum_a.positive?
           result_a -= sum_a
           sum_a = 0
         end
-      when "A"
+      when 'A'
         sum_b -= @amount[index]
         sum_a += @amount[index]
-        unless sum_b > 0
+        unless sum_b.positive?
           result_b -= sum_b
           sum_b = 0
         end
       end
     end
-    [result_a,result_b]
+
+    [result_a, result_b]
   end
 end
-
-# test =  BankTransfer.new('ABABB', [3, 1, 2, 2, 4])
-# puts test.min_initial_amount.inspect
